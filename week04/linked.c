02f1d64f2d16a85e7bd8eea3896bb0d1cf4b0bb1
John <johnbower2012@gmail.com>
2018-12-26 19:09:26 -0500

    dictionary

--- week04/linked.c
+++ week04/linked.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "struct.h"
+#include "linkedlist.h"
 
 int main(void)
 {
--- week04/tree.c
+++ week04/tree.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "struct.h"
+#include "binarytree.h"
 
 int main(void)
 {
